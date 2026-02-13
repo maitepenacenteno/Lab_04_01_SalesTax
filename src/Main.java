18af7d5e88e995df82f9785f60e290c16befcb0d
Maite  Peña <118402642+maitepena@users.noreply.github.com>
2026-02-12 22:33:05 -0500

    Task 1 - Sales Tax

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 20.5;
+        double taxRate = 0.05;
+        double taxAmount = price * taxRate;
+        System.out.println("The price of the item is: $" + price + " and the tax amount is: $" + taxAmount);
+    }
+}
